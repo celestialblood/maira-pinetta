47c30008c8562c5951af52e8281e887b653298d6
celestialblood <mairapinetta@gmail.com>
2022-07-29 20:34:57 -0300

    modificar carrito sumar y restar ARREGLADO

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 $(document).ready(function() { //documento listo corre por todo el codigo antes
 
     var num=0; //variable contador producto
-  
-    $('#minus').click(function() { //para clase .minus funcion restar producto
+    var iconfull = document.getElementById("icon-full");
+    var iconempty = document.getElementById("icon-empty");
+
+    $(".minus").click(function() { //para clase .minus funcion restar producto
       num=num-1;
       $(".num").text(num);
       check(); //voy a la funcion check
     });
   
-    $('#add').click(function() { //para la clase add funcion agregar producto
+    $(".add").click(function() { //para la clase add funcion agregar producto
       num=num+1;
       $(".num").text(num);
       check(); 
+      console.log(num)
     });
   
     function check() {
       if (num < 1) { // si el numero es menor a 1 modifico la clase del icono para visualizarlo  vacio
-        $(".fa-solid").removeClass("fa-cart-arrow-down");
+       // $(".fa-solid").removeClass("fa-cart-arrow-down");
+        iconempty.classList.remove('iconhidden');
+        iconfull.classList.add('iconhidden');
+
       } else if (num > 0 ) { //si el numero es mayor a 0 modifico la clase del icono para visualizarlo lleno
-        $(".fa-solid").removeClass("fa-cart-shopping");
-        $(".fa-solid").addClass("fa-cart-arrow-down");
+        iconfull.classList.remove('iconhidden');
+        iconempty.classList.add('iconhidden');
+       // $(".fa-solid").removeClass("fa-cart-shopping");
+       // $(".fa-solid").addClass("fa-cart-arrow-down");
       }
     }
     
