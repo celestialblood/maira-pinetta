dc0da79a1218ab3707aea3c648a5d72946bdafbf
celestialblood <mairapinetta@gmail.com>
2022-08-20 22:20:56 -0300

    sjdff

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-$(document).ready (function() { //documento listo corre por todo el codigo antes
+$(document).ready (function() { //funcion documento listo corre por todo el codigo antes
 
-  //seteo storage del carrito lo guardamos en cart //esto va siempre al principio
+  //ESTO SIEMPRE VA AL PRINCIPIO, variable global
+  //seteo el storage del carrito lo guardamos en cart // aca traigo si hay algo en el storage al carrito 
   // ?? significa que si esto es null o undefined  se va a guardar esto-> [] seteo en 0, si no puedo hacerlo con un if
   const cart= JSON.parse(localStorage.getItem('cart')) ?? []; 
   //el reduce cuenta los elementos y ir acumulando los valores y los suma
@@ -9,44 +10,75 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
   document.getElementById("cart-total").innerHTML =`${cart.length}`; // modifico su numero por cartlenght (cantidad productos del carro + el storage)
   document.getElementById("subtotal").innerHTML = `${subtotal}`; //Modifico el subtotal
   
-  //llevo los items al carrito del html
-  cart.forEach((product) => {
-    document.getElementById("cartWrapper").innerHTML += `
-    <div class="cart-item">
-      <img src="${product.img}">
-      <div class="details">
-        <h4 class="item-name">${product.title}</h4>
-        <p>Descripción
-          <span class="quantity">${cart.length}</span>
-          <span class="price"> $${product.price}</span>
-        </p>
-      </div>
-      <div class="cancel "><i class="fa-solid fa-xmark minus"></i></div>
-  </div>`
-  });
-  
   
   //ARRAY OF PRODUCTS
   const products =[
 
-    {class:"horizontal", id:"hidden-place", title:"Hidden place - A4", price:1000, img:"../Assets/Pinetta_Maira__DSC_0396.jpg"},
-    {class:"horizontal", id:"iridiscent-dreams", title:"Iridiscent dreams - A3", price:1500, img:"../Assets/20x30_Collage.jpg"},
-    {class:"horizontal", id:"refugio", title:"Refugio - A3", price:1500, img:"../Assets/A5_edit2.jpg"},
-    {class:"vertical", id:"home", title:"Home - A4", price:1000, img:"../Assets/10x15_6890.jpg" },
-    {class:"vertical", id:"anhelo", title:"Anhelo - A4", price:1000, img: "../Assets/Pinetta_Maira_6974.jpg"},
-    {class:"vertical", id:"soft", title:"Soft - A3", price:1500, img:"../Assets/MairaPinetta_Balam5.jpg"}
+    {class:"horizontal", id:"hidden-place", title:"Hidden place", price:1000, img:"../Assets/Pinetta_Maira__DSC_0396.jpg", size:"A4"},
+    {class:"horizontal", id:"iridiscent-dreams", title:"Iridiscent dreams", price:1500, img:"../Assets/20x30_Collage.jpg", size:"A3"},
+    {class:"horizontal", id:"refugio", title:"Refugio", price:1500, img:"../Assets/A5_edit2.jpg", size:"A3"},
+    {class:"vertical", id:"home", title:"Home", price:1000, img:"../Assets/10x15_6890.jpg", size:"A4"},
+    {class:"vertical", id:"anhelo", title:"Anhelo", price:1000, img: "../Assets/Pinetta_Maira_6974.jpg", size:"A4"},
+    {class:"vertical", id:"soft", title:"Soft", price:1500, img:"../Assets/MairaPinetta_Balam5.jpg", size:"A3"}
 
   ]
 
-  products.forEach((product) => { //for each producto del array productos hago las cards en el html desde js
-    const idButton = `add-cart${product.id}`
+  //FOR  para generar un array de los elementos con la clasee buttonProduct  con el for.each no se puede ?
+  //con esto hago que sea dinamico el evento  en vez de ir por id 
+  for (const nodeHTML of document.getElementsByClassName("buttonProduct")){
+
+    //si hay click sobre alguno de esos elementos (nodos) extraigo el atributo "data-size" (a4, a3 etc)
+    nodeHTML.addEventListener("click", (event) => {
+      const sizeProduct= event.target.getAttribute("data-size");
+      //ejecuto la funcion pasandole el parametro de size del elemento que se toco ej :A4 
+      filterProducts(sizeProduct);
+    });
+  }
+
+  
+  //FUNCION para filtrar productos
+  function filterProducts(sizeProduct) {
+    //vacio las cards ya generadas en el html 
+    document.getElementById("box-container-shop").innerHTML = " ";
+
+    //en una variable  guardo  la accion producto.filter  para filtrar por el parametro que obtuve anteriormente de "data-size"
+    const filteredProducts = products.filter ((product) => product.size === sizeProduct);
+
+    //traigo la variable y  genero las cards a partir del dato de esta
+    filteredProducts.forEach((product) => { 
+      const idButton = `add-cart${product.id}`
+      document.getElementById("box-container-shop").innerHTML += `<div class="box">
+        <div class="image">
+          <img class="${product.class}" src="${product.img}">
+        </div>
+  
+        <div class="info"> 
+          <h3 class="title"> ${product.title}</h3>    
+          <div class="subInfo">
+            <div class="price">${product.price}<i class="bi bi-flower2"></i></div>                        
+          </div>
+        </div>
+  
+        <div class="overlay">
+          <a href="#" style="--i:1;" id= ${idButton} class="add fas fa-shopping-cart"></a>
+          <a href="#" style="--i:2;" class="fas fa-heart"></a>
+          <a href="#" style="--i:4;" class="fas fa-search"></a>
+        </div>
+        
+      </div>`;
+    });  
+  }
+
+  //for each producto del array productos hago las cards en el html 
+  products.forEach((product) => { 
+    const idButton = `add-cart${product.id}` // string y el dato de cada producto que contiene el boton de agregar al carrito
     document.getElementById("box-container-shop").innerHTML += `<div class="box">
       <div class="image">
         <img class="${product.class}" src="${product.img}">
       </div>
 
       <div class="info"> 
-        <h3 class="title"> ${product.title}</h3>    
+        <h3 class="title"> ${product.title}-${product.size}</h3>    
         <div class="subInfo">
           <div class="price">${product.price}<i class="bi bi-flower2"></i></div>                        
         </div>
@@ -62,16 +94,14 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
 
   })
 
-  var num=0; //variable contador producto
-  var iconfull = document.getElementById("icon-full");
-  var iconempty = document.getElementById("icon-empty");
-  
 
  //FUNCION AGREGO PRODUCTOS AL CARRITO Y SUMO EL  LOCAL STORAGE y sumo el SUBTOTAL
   products.forEach((product) => {  
     const idButton = `add-cart${product.id}`; //cuando hago click ne el boton se agrega a carrito la cantidad 
     document.getElementById(idButton).addEventListener('click', () =>{
       cart.push(product); //llevo el producto al carro   
+      console.log (cart);
+
       localStorage.setItem('cart', JSON.stringify(cart)); //seteo el carro al storage y cambio el valor a string para llevarlo al storage de nuevo
       //el reduce cuenta los elementos y ir acumulando los valores y los suma
       const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
@@ -79,12 +109,74 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
       //llevo al html la cantidad de productos y el total de estos a cart-total y a subtotal
       document.getElementById("cart-total").innerHTML =`${cart.length}`; // 
       document .getElementById("subtotal").innerHTML = `${subtotal}`; 
+
+      updateCart();
     });
 
   });
 
 
+  //FUNCION ELIMINAR DEL CARRITO
+  const removeFromCart= () =>{
+    
+    document.getElementById(removeButton).addEventListener('click', (event)=>{ console.log(event.target.getAttribute('data-id'))
 
+      let cartProductId = event.target.getAttribute('data-id')
+    
+      //busco el producto con find, la condicion es que el id sea igual al id del boton de ese producto 
+      let productFound = cart.find ((product) => product.id === cartProductId)
+      //creo el indice del producto con la info del item de arriba
+      const indice = cart.indexOf(productFound); //o findindex
+      //splice elimina y  recibe 2 parametros indice y la cantidad de elemntos a borrar
+      cart.splice(indice, 1);
+
+      updateCart();
+    
+    });
+
+  };
+
+ //FUNCION ACTUALIZAR CARRITO 
+  const updateCart= () =>{
+    //borra el nodo y actualiza el carro
+    document.getElementById("cartWrapper").innerHTML = ""
+
+    //genero los productos en el carrito
+    cart.forEach((product) => {
+        
+      document.getElementById("cartWrapper").innerHTML += `
+      <div class="cart-item">
+        <img src="${product.img}">
+        <div class="details">
+          <h4 class="item-name">${product.title}</h4>
+          <p>Descripción
+            <span class="quantity">${cart.length}</span>
+            <span class="price"> $${product.price}</span>
+          </p>
+        </div>   
+        <div class="cancel "><i  id="removeButton" data-id=“${product.id}” class="fa-solid fa-xmark minus"></i></div>
+      </div>`
+
+      removeFromCart();
+    });
+
+    localStorage.setItem('cart', JSON.stringify(cart)); //seteo el carro al storage y cambio el valor a string para llevarlo al storage de nuevo
+    const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
+    
+    document.getElementById("cart-total").innerHTML =`${cart.length} - ${subtotal}`; // 
+    document .getElementById("subtotal").innerHTML = `${subtotal}`; 
+
+  }
+
+  
+  
+   
+
+  let num=0; //variable contador producto
+  let iconfull = document.getElementById("icon-full");
+  let iconempty = document.getElementById("icon-empty");
+  
+ //MODIFICO numero de productos del carrito (icono)
   $(".minus").click(function() { //para clase minus funcion restar producto
     num=num-1;
     $(".num").text(num);
@@ -97,7 +189,8 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
     check(); 
     console.log(num)
   });
-
+  
+  //modifico icono del carrito
   function check() { //funcion checkear cantidad de productos
     if (num < 1) { // si el numero es menor a 1 modifico la clase del icono para visualizarlo  vacio
      // $(".fa-solid").removeClass("fa-cart-arrow-down");
@@ -118,6 +211,7 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
   const cartIcon = document.querySelector('.icon-cart') //selecciono icono del nav 
   const wholeCartWindow = document.querySelector('.whole-cart-window');  //selecciono ventana carrito
   
+  //muestro y oculto carrito 
   cartIcon.addEventListener('click', () => { //si hay un click sobre el icon-cart disparo esta función
 
     wholeCartWindow.classList.toggle('hide-it'); //borra clase hide it y la muestra dependiendo el click
@@ -125,4 +219,4 @@ $(document).ready (function() { //documento listo corre por todo el codigo antes
   })
 
 
-  });
+});
