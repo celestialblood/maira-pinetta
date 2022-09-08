893f178883e2e30053abc0e4e4cc2bf641faaebc
celestialblood <mairapinetta@gmail.com>
2022-09-08 00:13:15 -0300

    FETCH CORREGIDO Y AGREGADO

--- js/main.js
+++ js/main.js
@@ -1,121 +1,115 @@
 $(document).ready (function() { //funcion documento listo corre por todo el codigo antes
 
-  //ESTO SIEMPRE VA AL PRINCIPIO, variable global
+  
+  //CART SIEMPRE VA AL PRINCIPIO, variable global
   //seteo el storage del carrito lo guardamos en cart // aca traigo si hay algo en el storage al carrito 
   // ?? significa que si esto es null o undefined  se va a guardar esto-> [] seteo en 0, si no puedo hacerlo con un if
   const cart= JSON.parse(localStorage.getItem('cart')) ?? []; 
   //el reduce cuenta los elementos y ir acumulando los valores y los suma
   const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
     
-  document.getElementById("cart-total").innerHTML =`${cart.length}`; // modifico su numero por cartlenght (cantidad productos del carro + el storage)
+  document.getElementById("cart-total").innerHTML =`${cart.length}`; // modifico su numero por cartlenght (cantidad products del carro + el storage)
   document.getElementById("subtotal").innerHTML = `${subtotal}`; //Modifico el subtotal
-  
-  
-  //ARRAY OF PRODUCTS
-  const products =[
 
-    {class:"horizontal", id:"hidden-place", title:"Hidden place", price:1000, img:"../Assets/Pinetta_Maira__DSC_0396.jpg", size:"A4"},
-    {class:"horizontal", id:"iridiscent-dreams", title:"Iridiscent dreams", price:1500, img:"../Assets/20x30_Collage.jpg", size:"A3"},
-    {class:"horizontal", id:"refugio", title:"Refugio", price:1500, img:"../Assets/A5_edit2.jpg", size:"A3"},
-    {class:"vertical", id:"home", title:"Home", price:1000, img:"../Assets/10x15_6890.jpg", size:"A4"},
-    {class:"vertical", id:"anhelo", title:"Anhelo", price:1000, img: "../Assets/Pinetta_Maira_6974.jpg", size:"A4"},
-    {class:"vertical", id:"soft", title:"Soft", price:1500, img:"../Assets/MairaPinetta_Balam5.jpg", size:"A3"}
 
-  ]
+  /*fetch json*/
+  const inicializar = async () => {
+    const products = await fetchProducts();
+    generarInterfaz(products);
+    return products;
+  };
 
-  //FOR  para generar un array de los elementos con la clasee buttonProduct  con el for.each no se puede ?
-  //con esto hago que sea dinamico el evento  en vez de ir por id 
+  const generarInterfaz = (responseFinal) => {
+    createCards(responseFinal);
+    addProduct(responseFinal);
+    renderCart(responseFinal);
+    filterBySize(responseFinal);
+    check(responseFinal);
+  }
+
+  const fetchProducts = async () => {
+    const response = await fetch ('../products.json');
+    const responseFinal = await response.json();
+    return responseFinal
+  }
+
+
   for (const nodeHTML of document.getElementsByClassName("buttonProduct")){
 
     //si hay click sobre alguno de esos elementos (nodos) extraigo el atributo "data-size" (a4, a3 etc)
     nodeHTML.addEventListener("click", (event) => {
       const sizeProduct= event.target.getAttribute("data-size");
       //ejecuto la funcion pasandole el parametro de size del elemento que se toco ej :A4 
-      filterProducts(sizeProduct);
+      filterBySize(sizeProduct);
     });
+    async function filterBySize (sizeProduct){
+      const products = await fetchProducts();
+      const productsBysize= products.filter(product => product.size === sizeProduct);
+      generarInterfaz(productsBysize);
+    }; 
   }
 
+
   
-  //FUNCION para filtrar productos
-  function filterProducts(sizeProduct) {
-    //vacio las cards ya generadas en el html 
-    document.getElementById("box-container-shop").innerHTML = " ";
+  inicializar();
 
-    //en una variable  guardo  la accion producto.filter  para filtrar por el parametro que obtuve anteriormente de "data-size"
-    const filteredProducts = products.filter ((product) => product.size === sizeProduct);
+  /* CARDS */
 
-    //traigo la variable y  genero las cards a partir del dato de esta
-    filteredProducts.forEach((product) => { 
-      const idButton = `add-cart${product.id}`
-      document.getElementById("box-container-shop").innerHTML += `<div class="box">
+  function createCards(cargarproducts){
+    let cards = "";
+    cargarproducts.forEach((product) => { 
+      let idButton 
+      idButton= `add-cart-${product.id}`
+      cards += `<div class="box">
         <div class="image">
           <img class="${product.class}" src="${product.img}">
         </div>
-  
+
         <div class="info"> 
-          <h3 class="title"> ${product.title}</h3>    
+          <h3 class="title"> ${product.title}-${product.size}</h3>    
           <div class="subInfo">
             <div class="price">${product.price}<i class="bi bi-flower2"></i></div>                        
           </div>
         </div>
-  
+
         <div class="overlay">
           <button href="#" style="--i:1;" id= ${idButton} class="add fas fa-shopping-cart"></button>
           <button href="#" style="--i:2;" class="fas fa-heart"></button>
           <button href="#" style="--i:4;" class="fas fa-search"></button>
         </div>
-        
+
       </div>`;
-    });  
+
+    });
+    document.getElementById("box-container-shop").innerHTML= cards;
+  };
+
+
+  /* add AL CARRITO */
+
+  function addProduct(cargarproducts) {
+
+    cargarproducts.forEach((product) =>{
+      idButton= `add-cart-${product.id}`;
+      document.getElementById(idButton).addEventListener('click', () =>{
+
+        cart.push(product); //llevo el product al carro   
+        localStorage.setItem('cart', JSON.stringify(cart)); //seteo el carro al storage y cambio el valor a string para llevarlo al storage de nuevo
+        //el reduce cuenta los elementos y ir acumulando los valores y los suma
+        const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
+
+        //llevo al html la cantidad de products y el total de estos a cart-total y a subtotal
+        document.getElementById("cart-total").innerHTML =`${cart.length}`; // 
+        document .getElementById("subtotal").innerHTML = `${subtotal}`; 
+        console.log(cart.length)
+        renderCart();
+
+      });
+    });
   }
 
- //rendereo el html del store
- products.forEach((product) => { 
-  let idButton 
-  idButton= `add-cart-${product.id}`
-  document.getElementById("box-container-shop").innerHTML += `<div class="box">
-    <div class="image">
-      <img class="${product.class}" src="${product.img}">
-    </div>
-
-    <div class="info"> 
-      <h3 class="title"> ${product.title}-${product.size}</h3>    
-      <div class="subInfo">
-        <div class="price">${product.price}<i class="bi bi-flower2"></i></div>                        
-      </div>
-    </div>
-
-    <div class="overlay">
-      <button href="#" style="--i:1;" id= ${idButton} class="add fas fa-shopping-cart"></button>
-      <button href="#" style="--i:2;" class="fas fa-heart"></button>
-      <button href="#" style="--i:4;" class="fas fa-search"></button>
-    </div>
-
-  </div>`;
-
-  })
-
-  // agrego un event listener para cada add-cart
-  products.forEach((product) =>{
-    idButton= `add-cart-${product.id}`;
-    document.getElementById(idButton).addEventListener('click', () =>{
-
-      cart.push(product); //llevo el producto al carro   
-      localStorage.setItem('cart', JSON.stringify(cart)); //seteo el carro al storage y cambio el valor a string para llevarlo al storage de nuevo
-      //el reduce cuenta los elementos y ir acumulando los valores y los suma
-      const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
-
-      //llevo al html la cantidad de productos y el total de estos a cart-total y a subtotal
-      document.getElementById("cart-total").innerHTML =`${cart.length}`; // 
-      document .getElementById("subtotal").innerHTML = `${subtotal}`; 
-      console.log(cart.length)
-    });
-
-
-  })
-
-  /// CARRITO
-  let num=cart.length; //variable contador producto
+  /// CARRITO ICON
+  let num=cart.length; //variable contador product
 
   const cartIcon = document.querySelector('.icon-cart') //selecciono icono del nav 
   const wholeCartWindow = document.querySelector('.whole-cart-window');  //selecciono ventana carrito
@@ -123,7 +117,7 @@ $(document).ready (function() { //funcion documento listo corre por todo el codi
   //muestro y oculto carrito 
   cartIcon.addEventListener('click', () => { //si hay un click sobre el icon-cart disparo esta función
     
-    rendercart(cart);
+    renderCart(cart);
     check();
     wholeCartWindow.classList.toggle('hide-it'); //borra clase hide it y la muestra dependiendo el click
   });
@@ -134,8 +128,8 @@ $(document).ready (function() { //funcion documento listo corre por todo el codi
   let iconempty = document.getElementById("icon-empty");    
 
   //Funciones
-  //modifica icono del carrito si esta vacio o con productos
-  function check() { //funcion checkear cantidad de productos
+  //modifica icono del carrito si esta vacio o con products
+  function check() { //funcion checkear cantidad de products
 
     if (num < 1) { // si el numero es menor a 1 modifico la clase del icono para visualizarlo  vacio
     // $(".fa-solid").removeClass("fa-cart-arrow-down");
@@ -150,8 +144,9 @@ $(document).ready (function() { //funcion documento listo corre por todo el codi
     }
   }
 
+
   // rendereo carrito y agregacion de listeners para el borrado 
-  function rendercart(cart) {
+  function renderCart(cart) {
 
     document.getElementById("cartWrapper").innerHTML = ''
 
@@ -194,21 +189,82 @@ $(document).ready (function() { //funcion documento listo corre por todo el codi
               'success'
             )
             cart.splice(index, 1)
-            rendercart(cart)
+            renderCart(cart)
             localStorage.setItem('cart', JSON.stringify(cart)); 
             const subtotal= cart.reduce((accumulator, product) => accumulator + product.price, 0);
-            //llevo al html la cantidad de productos y el total de estos a cart-total y a subtotal
+            //llevo al html la cantidad de products y el total de estos a cart-total y a subtotal
             document.getElementById("cart-total").innerHTML =`${cart.length}`; // 
             document .getElementById("subtotal").innerHTML = `${subtotal}`; 
           }
         })
-       
+      
       })
-    })
+    });
 
 
 
   }
+  // /* BORRAR products */
+
+  // function borrarproduct(cargarproducts) {
+  //   cargarproducts.forEach((product) => {
+  //   const idBotonDelete = `delete-cart${product.id}`;
+  //   document.getElementById(idBotonDelete).addEventListener("click", () => {
+  //     const productAEliminar = productsEnCarrito.findIndex(
+  //       (products) => products.id === product.id
+  //     );
+  //     console.log(productAEliminar + "posición product a eliminar");
+  //     if (productAEliminar !== -1) {
+  //       productsEnCarrito.splice(productAEliminar, 1);
+  //     }
+  //     localStorage.setItem("totalCarrito", JSON.stringify(productsEnCarrito));
+  //     console.log(productsEnCarrito);
+  //     document.getElementById("btnCarritoTotal").innerHTML = `${productsEnCarrito.length}`;
+  //     Toastify({
+  //         text: "Eliminaste " + `${product.nombre}` + " del carrito",
+  //         duration: 3000,
+  //         gravity: "bottom",
+  //         style: {
+  //           background: "rgb(228, 98, 141)",
+  //         },
+  //       }).showToast();
+  //       renderCarrito();
+  //       renderTotalCarrito();
+  //   });
+  //  });
+  // };
+
+  // /* CARRITO POPUP */
+
+  // function renderCarrito() {
+  //   let cardsPopUp = "";
+  //   productsEnCarrito.forEach(({id, nombre, precio, imagen}) => {
+  //     cardsPopUp += `<div class="cardTest">
+  //               <div>
+  //               <img src='${imagen}' class="imagenCardsPopup">
+  //               </div>
+  //               <h2>${nombre}</h2>
+  //               <h4>$${precio}</h4>
+  //               </div>`;                       
+  //   });
+  //   document.getElementById("productsAgregados").innerHTML = cardsPopUp;
+  // };
+
+  // /* TOTAL CARRITO POPUP */
+
+  // let totalCarritoPopUp ="";
+
+  // function renderTotalCarrito() {
+  //   const totalCarrito = productsEnCarrito.reduce(
+  //     (acumulador, product) => acumulador + product.precio, 0
+  //     );
+  //     totalCarritoPopUp =`<div class="cardTest separador">
+  //                       <h2>TOTAL: </h2>  
+  //                       <h4>$${totalCarrito}</h4>
+  //                       </div>`;
+  //   document.getElementById("totalCompra").innerHTML = totalCarritoPopUp;
+  // };
+
 
 });
 
